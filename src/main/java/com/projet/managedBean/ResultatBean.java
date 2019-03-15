ade58141fa8b3db7bf4265fde8b44c66de0e068c
Nanometra <npmmul69@gmail.com>
2019-03-15 16:14:22 +0100

    Configuration web.xml, faces-config.xml et ajout d'un managedBean

---
+++ src/main/java/com/projet/managedBean/ResultatBean.java
@@ -0,0 +1,22 @@
+package com.projet.managedBean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="resultatBean")
+@RequestScoped
+public class ResultatBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String resultat() {
+		System.out.println("Dans le bean ResultatBean");
+		return "success";
+	}
+	
+}
