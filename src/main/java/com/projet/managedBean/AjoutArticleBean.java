862cb97fc555fb860e659b543990dc9e1744156a
Nanometra <npmmul69@gmail.com>
2019-03-18 16:28:15 +0100

    Mise en place de la vue sur différentes pages xhtml.

---
+++ src/main/java/com/projet/managedBean/AjoutArticleBean.java
@@ -0,0 +1,18 @@
+package com.projet.managedBean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class AjoutArticleBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+}
---
+++ src/main/java/com/projet/vue/FileUploadVue.java
@@ -0,0 +1,40 @@
+package com.projet.vue;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+@ManagedBean(name="fileUploadVue")
+public class FileUploadVue {
+	
+	private UploadedFile file;
+
+	public FileUploadVue(UploadedFile file) {
+		super();
+		this.file = file;
+	}
+
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
+	}
+	
+	public void uplaod() {
+		if (file != null) {
+			FacesMessage message = new FacesMessage("Succes", file.getFileName() + "est uploadé.");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+		}
+	}
+	
+	public void handleFileUpload(FileUploadEvent event) {
+		FacesMessage msg = new FacesMessage("Succes", event.getFile().getFileName() + " est uploadé.");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+}
