32e528ab23e92a80ded21610e8caf328622664c8
Noemie <npmmul69@gmail.com>
2019-03-21 01:28:26 +0100

    En cours - DAOFactory pour charger les données des properties file dans persistence.xml

--- src/main/java/com/projet/dao/DAOFactory.java
+++ src/main/java/com/projet/dao/DAOFactory.java
@@ -1,5 +1,6 @@
 package com.projet.dao;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,9 +13,14 @@ public class DAOFactory {
 
 	private static final String FICHIER_JDBC = "../resources/com/projet/properties/db.properties";
 	private static final String FICHIER_HIBERNATE = "../resources/com/projet/properties/hibernate.properties";
-	private static final String PROPERTY_URL = "hibernate.connection.url";	
-	private static final String PROPERTY_DRIVER = "hibernate.connection.driver_class";	
-	private static final String PROPERTY_UTILISATEUR = "hibernate.connection.url";	
+	private static final String PROPERTY_URL = "javax.persistence.jdbc.url";	
+	private static final String PROPERTY_JDBC_URL = "jdbc.url";
+	private static final String PROPERTY_DRIVER = "javax.persistence.jdbc.driver";	
+	private static final String PROPERTY_UTILISATEUR = "javax.persistence.jdbc.user";	
+	private static final String PROPERTY_MOT_DE_PASSE = "javax.persistence.jdbc.password";	
+	private static final String PROPERTY_HIBERNATE_DIALECT = "hibernate.dialect";	
+	private static final String PROPERTY_HIBERNATE_SQL = "hibernate.show_sql";	
+//	private static final String PROPERTY_HIBERNATE_HBM2DDL = "javax.persistence.schema-generation.database.action";	
 	
 	
 	public static DAOFactory getInstance() throws DAOConfigurationException {
@@ -32,13 +38,22 @@ public class DAOFactory {
 			throw new DAOConfigurationException("Le fichier " + FICHIER_HIBERNATE + " est introuvable."); 
 		}
 		
-		props.load();
-		
 		Map<String, String> properties = new HashMap<String, String>();
+		
+		try {
+		props.load(fichierJdbc);
+		properties.put(PROPERTY_URL, props.getProperty(PROPERTY_URL));
+		
+		
+		
 //		properties
+		} catch (IOException e) {
+			throw new DAOConfigurationException("Impossible de charger les fichiers de propriétés", e);
+		}
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("GamesCornerV2_PU", properties);
-
+		
+		
 	}
 	
 }
