a5bc677a327fe642e41155aa62223b3e2da800d2
Nanometra <npmmul69@gmail.com>
2019-03-25 15:05:55 +0100

    Terminé - Mise en place de la variabilisation du fichier de persistence.xml

--- src/main/java/com/projet/dao/DAOFactory.java
+++ src/main/java/com/projet/dao/DAOFactory.java
@@ -4,14 +4,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
-import java.util.Set;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -24,23 +21,19 @@ public class DAOFactory {
 
 		Map<String, String> properties = new HashMap<String, String>();
 		Map<Integer, String> keys = new HashMap<Integer, String>();
-		Map<Integer, String> value = new HashMap<Integer, String>();
+		Map<Integer, String> concat = new HashMap<Integer, String>();
+		List cles = new ArrayList();
 
 		try {
 			// Chargement du fichier contenant le nom des propriétés.
 			props.load(fichierBase);
-			keys = getListeValeur(props, keys);
+			keys = getListeValeur(props, keys, cles);
 
 			props.clear();
 
-			for (int i = 0; i < inputStreams.length; i++) {
-				props.load(inputStreams[i]);
+			concat = mergeFichiers(props, cles, inputStreams);
 
-				value = getListeValeur(props, value);
-				props.clear();
-			}
-
-			properties = getProperties(keys, value, properties);
+			properties = getProperties(keys, concat, properties);
 
 		} catch (IOException e) {
 			throw new DAOConfigurationException("Impossible de charger les fichiers de propriétés", e);
@@ -77,11 +70,9 @@ public class DAOFactory {
 
 	// Méthode au sein de la classe qui permet de récupérer les valeurs de chaques
 	// propriétés
-	private static Map<Integer, String> getListeValeur(Properties props, Map<Integer, String> value) {
-//		Set<Entry<Object, Object>> valeurs = props.entrySet();
-//		Enumeration<Object> valeurs = props.elements();
-		List keys = new ArrayList(props.keySet());
-		Collections.sort(keys);
+	private static Map<Integer, String> getListeValeur(Properties props, Map<Integer, String> value, List cles) {
+
+		List keys = trierMap(props, cles);
 
 		int i = 0;
 		if (value.size() == 0) {
@@ -100,30 +91,46 @@ public class DAOFactory {
 		return value;
 	}
 
-//	private static Map<Integer, String> mergeFichiers(Properties props,
-//			InputStream... inputStream) throws IOException {
-//
-//		Map<Integer, String> concat = new HashMap<Integer, String>();
-//		Map<Integer, String> value = new HashMap<Integer, String>();
-//		
-//		for (int i = 0; i < inputStream.length; i++) {
-//			props.load(inputStream[i]);
-//			value = props.e
-//			for (int j = concat.size(); j < value.length; i++) {
-//				
-//			}
-//			props.clear();
-//		}
-//		return null;
-//	}
+	// Permet de trier les propriétés selon leur hashage
+	private static List trierMap(Properties props, List cles) {
+		// Range les clés dans leur ordre de hashage
+		if (cles.isEmpty()) {
+			cles = new ArrayList(props.keySet());
+			Collections.sort(cles);
+		} else {
+			for (int i = 0 ; i < cles.size() ; i++) {
+				cles.add(cles.get(i));
+			}
+			Collections.sort(cles);
+		}
+		
+		return cles;
+	}
 
-	private static Map<String, String> getProperties(Map<Integer, String> keys, Map<Integer, String> value,
+	// Méthode qui permet de merger les données de plusieurs fichiers de propriétés
+	// et de les trier selon leur ordre de hashage.
+	private static Map<Integer, String> mergeFichiers(Properties props, List cles,
+			InputStream... inputStream) throws IOException {
+
+		Map<Integer, String> value = new HashMap<Integer, String>();
+
+		for (int i = 0 ; i < inputStream.length ; i++) {
+			props.load(inputStream[i]);
+		}
+		
+		value = getListeValeur(props, value, cles);
+		props.clear();
+
+		return value;
+	}
+
+	private static Map<String, String> getProperties(Map<Integer, String> keys, Map<Integer, String> concat,
 			Map<String, String> properties) {
 
-		if (keys.size() == value.size()) {
-			if (keys.size() == value.size()) {
-				for (int i = properties.size() ; i < keys.size(); i++) {
-					properties.put(keys.get(i), value.get(i));
+		if (keys.size() == concat.size()) {
+			if (keys.size() == concat.size()) {
+				for (int i = properties.size(); i < keys.size(); i++) {
+					properties.put(keys.get(i), concat.get(i));
 				}
 			} else {
 				throw new DAOConfigurationException("La taille des 2 Map n'est pas la même.");
