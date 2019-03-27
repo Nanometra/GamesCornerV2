f2277dc572b0db5745f09c8b6fe6685519be422d
Nanometra <npmmul69@gmail.com>
2019-03-27 10:31:31 +0100

    ReCommit

--- src/main/java/com/projet/managedBean/InscriptionBean.java
+++ src/main/java/com/projet/managedBean/InscriptionBean.java
@@ -9,5 +9,19 @@ public class InscriptionBean {
 
 	private String login = "";
 	private String password = "";
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 	
 }
