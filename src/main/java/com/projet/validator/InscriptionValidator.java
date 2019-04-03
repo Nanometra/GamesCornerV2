d5f4333e05af8964373f4e5a1041ed5ac3367afe
Noemie <npmmul69@gmail.com>
2019-04-04 00:55:27 +0200

    En cours - Validation de l'inscription

--- src/main/java/com/projet/validator/InscriptionValidator.java
+++ src/main/java/com/projet/validator/InscriptionValidator.java
@@ -1,5 +1,28 @@
 package com.projet.validator;
 
-public class InscriptionValidator {
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
 
+@ManagedBean
+@RequestScoped
+@FacesValidator
+public class InscriptionValidator implements Validator {
+
+	private String email;
+
+	public InscriptionValidator() {
+		super();
+	}
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+				
+	}
+	
+	
 }
