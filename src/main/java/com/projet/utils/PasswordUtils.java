f8b6135ed8f10182fa4f350b7cfcbacea2c03654
Nanometra <npmmul69@gmail.com>
2019-04-15 16:33:51 +0200

    En cours - Mise en place de la protection des mots de passe

---
+++ src/main/java/com/projet/utils/PasswordUtils.java
@@ -0,0 +1,20 @@
+package com.projet.utils;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+public class PasswordUtils {
+
+	private Logger
+	
+	private static generateStrongPasswordHash (String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
+		
+		
+		
+	}
+	
+	private static byte[] getSalt() throws NoSuchAlgorithmException {
+		
+	}
+	
+}
