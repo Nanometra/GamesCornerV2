ff6d6916a8cd51a187c906af69bd864e1f4eccd7
Noemie <npmmul69@gmail.com>
2019-05-20 00:51:37 +0200

    WIP - Mise en place de la page d'info sur le client. Suppression des fichiers inutiles de test.

---
+++ src/main/java/com/projet/managedBean/InfoUtilisateurBean.java
@@ -0,0 +1,19 @@
+package com.projet.managedBean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class InfoUtilisateurBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
