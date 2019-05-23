979a585f766bbe6d451f8afb5b7425f0fa78f587
Nanometra <npmmul69@gmail.com>
2019-05-23 16:47:12 +0200

    Commit pour branche

--- src/main/java/com/projet/managedBean/InfoUtilisateurBean.java
+++ src/main/java/com/projet/managedBean/InfoUtilisateurBean.java
@@ -3,10 +3,15 @@ package com.projet.managedBean;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
 
+import com.projet.commons.DAOUtils;
+import com.projet.dao.IUtilisateurDAO;
+import com.projet.entites.Utilisateur;
+
 @ManagedBean
-@ViewScoped
+@RequestScoped
 public class InfoUtilisateurBean implements Serializable {
 
 	/**
@@ -14,6 +19,30 @@ public class InfoUtilisateurBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private Utilisateur utilisateur;
+	private IUtilisateurDAO utilisateurDAO;
 	
+	public InfoUtilisateurBean() {
+		super();
+		this.utilisateurDAO = DAOUtils.getUtilisateurDAO(); 
+	}
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+	
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+	
+	public IUtilisateurDAO getUtilisateurDAO() {
+		return utilisateurDAO;
+	}
+	
+	public void setUtilisateurDAO(IUtilisateurDAO utilisateurDAO) {
+		this.utilisateurDAO = utilisateurDAO;
+	}
+	
+		
 
 }
